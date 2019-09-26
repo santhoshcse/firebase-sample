45f262794964535f990b7fa38db034da9d35dbb5
ANALANCE\Santhosh.Kumar <Santhosh.Kumar@ducenIT.com>
2019-09-26 20:38:52 +0530

    Added Basic CRUD app - Read

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-sample';
+
+  public books: Observable<any[]>;
+
+  private bookCounter = 0;
+
+  constructor(public firestore: AngularFirestore) {
+      this.books = this.firestore.collection<Book>('/books').valueChanges();
+  }
+
+  add() {
+    this.firestore
+    .collection('books')
+    .add({ title: 'book ' + this.bookCounter++ })
+    .then(res => {}, err => reject(err));
+  }
+
+  delete(book) {
+    this.firestore
+    .collection('books')
+    .doc(book.payload.doc.id)
+    .delete();
+  }
+
+  get() {
+    // TODO
+  }
+
+  update() {
+    // TODO
+  }
+}
+
+class Book {
+  title: string;
+  constructor(title: string) {
+    this.title = title;
+  }
 }
